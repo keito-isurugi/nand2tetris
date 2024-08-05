1ac41c8710b8c25a550a368559d1939ee10710f9
keito-isurugi <ja224kg@gmail.com>
2024-08-05 21:02:26 +0900

    4.機械語 乗算

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// それぞれ初期化
+    @R2
+    M=0 // R2=0
+    @i
+    M=1 // i=1
+(LOOP)
+    @i
+    D=M // D=i
+    @R1
+    D=D-M // D=i-R1
+    @END
+    D;JGT
+    @R0
+    D=M // D=R0
+    @R2
+    M=D+M // R2=R0+R2
+    @i
+    M=M+1 // i++
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
